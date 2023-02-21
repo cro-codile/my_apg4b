72eb6b2342ff76213c2d22b33e73c6ef58b28d38
kaomi <minami4349@gmail.com>
2023-02-21 13:27:04 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+
 int main() {
 
-  int n,a;
+   // 288-A
 
-  string ans = "No";
-  cin >> n >> a;
 
-  int mod = n % 500;
-  if(mod == 0 || a >= mod){
-    ans = "Yes";
-  }
+  
 
-  cout << ans << endl;
+
+  
 }
